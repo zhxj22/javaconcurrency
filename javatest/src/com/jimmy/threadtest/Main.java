5387ea2ece4dbf753e6cfa5cf9c6bfb392cb1532
xzhao23 <xzhao23@Jimmy-Win7>
2016-03-26 15:33:17 +0800

    how to interrup a thread

--- javatest/src/com/jimmy/threadtest/Main.java
+++ javatest/src/com/jimmy/threadtest/Main.java
@@ -6,49 +6,18 @@ import java.io.PrintWriter;
 public class Main {
 	
 	public static void main(String[] args) {
-		Thread threads[] = new Thread[10];
-		Thread.State stats[] = new Thread.State[10];
-		for (int i = 0; i < 10; i++) {
-			threads[i] = new Thread(new Calculator(i));
-			threads[i].setName("Thread --" + i);
-			if (i%2 == 0) {
-				threads[i].setPriority(Thread.MAX_PRIORITY);
-			}
-			else{
-				threads[i].setPriority(Thread.MIN_PRIORITY);
-			}
+		
+		//how to interrupt threads
+		Thread infiniteThread = new Thread(new InfiniteTask());
+		infiniteThread.start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
 		}
-		try (FileWriter fw = new FileWriter(".\\log.txt");PrintWriter pw = new PrintWriter(fw)){
-			for (int i=0; i<10; i++){
-				pw.println("Main : Status of Thread "+i+" : " + threads[i].getState());
-				stats[i]=threads[i].getState();
-			}
-			for (int i=0; i<10; i++){
-				threads[i].start();
-			}
-			boolean finish=false;
-			while (!finish) {
-			for (int i=0; i<10; i++){
-				if (threads[i].getState()!=stats[i]) {
-					writeThreadInfo(pw, threads[i],stats[i]);
-					stats[i]=threads[i].getState();
-				}
-			}
-			finish=true;
-			for (int i=0; i<10; i++){
-				finish=finish &&(threads[i].getState()==Thread.State.TERMINATED);
-			}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	private static void writeThreadInfo(PrintWriter pw, Thread
-		thread, Thread.State state) {
-		pw.printf("Main : Id %d - %s\n",thread.getId(),thread.getName());
-		pw.printf("Main : Priority: %d\n",thread.getPriority());
-		pw.printf("Main : Old State: %s\n",state);
-		pw.printf("Main : New State: %s\n",thread.getState());
-		pw.printf("Main : ************************************\n");
+		infiniteThread.interrupt();
+		System.out.println(infiniteThread.isInterrupted());
+		System.out.println(infiniteThread.getState());
 	}
 }
