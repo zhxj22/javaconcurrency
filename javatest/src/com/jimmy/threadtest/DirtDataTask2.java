551320d16b19b483e415c88c18f5c0b59eb247f4
xzhao23 <xzhao23@Jimmy-Win7>
2016-03-26 23:01:41 +0800

    another use of synchronized keyword. it is not on a method but only a code block

---
+++ javatest/src/com/jimmy/threadtest/DirtDataTask2.java
@@ -0,0 +1,51 @@
+package com.jimmy.threadtest;
+
+import java.util.concurrent.TimeUnit;
+
+public class DirtDataTask2 {
+
+	public static void main(String ars[]){
+		Data d = new Data();
+		new Thread(new OptTask(d)).start();
+		new Thread(new OptTask(d)).start();
+		new Thread(new OptTask(d)).start();
+		new Thread(new OptTask(d)).start();
+		new Thread(new OptTask(d)).start();
+		new Thread(new OptTask(d)).start();
+		
+	}
+	
+	public static class OptTask implements Runnable{
+		Data d;
+		public OptTask(Data d) {
+			// TODO Auto-generated constructor stub
+			this.d = d;
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			d.operate();
+		}
+		
+	}
+	public static class Data{
+		private int a = 1;
+		//when ahve the synchronized key word, the thread running 
+		//this code would pending till another therad finish this code
+		public void operate(){
+			synchronized (this) {
+				a --;
+				System.out.println("I am thread: " + Thread.currentThread());
+				try {
+					TimeUnit.SECONDS.sleep(1);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("value of a should be 0, actual is " + a);
+				a ++;
+			}
+			
+		}
+	}
+}
