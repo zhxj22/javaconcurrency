623004d8c2bf9e91121c99502ba5b161426195c2
xzhao23 <xzhao23@Jimmy-Win7>
2016-03-26 15:07:28 +0800

    monitor the status of threads

--- javatest/src/com/jimmy/threadtest/Main.java
+++ javatest/src/com/jimmy/threadtest/Main.java
@@ -1,23 +1,54 @@
 package com.jimmy.threadtest;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		for (int i = 0; i <10; i++) {
-			Calculator calculator=new Calculator(i);
-			Thread thread=new Thread(calculator);
-			thread.setPriority(Thread.MAX_PRIORITY);
-			thread.start();
+		Thread threads[] = new Thread[10];
+		Thread.State stats[] = new Thread.State[10];
+		for (int i = 0; i < 10; i++) {
+			threads[i] = new Thread(new Calculator(i));
+			threads[i].setName("Thread --" + i);
+			if (i%2 == 0) {
+				threads[i].setPriority(Thread.MAX_PRIORITY);
+			}
+			else{
+				threads[i].setPriority(Thread.MIN_PRIORITY);
+			}
 		}
-		
-		Thread th = new Thread(new MyRunnable());
-		th.start();
-		Thread th2 = new Thread(new MyRunnable());
-		th2.start();
-		Thread th3 = new Thread(new MyRunnable());
-		th3.start();
-		MyThread th4 = new MyThread();
-		th4.start();
-		System.out.println("The main thread ends:" + Thread.currentThread());
+		try (FileWriter fw = new FileWriter(".\\log.txt");PrintWriter pw = new PrintWriter(fw)){
+			for (int i=0; i<10; i++){
+				pw.println("Main : Status of Thread "+i+" : " + threads[i].getState());
+				stats[i]=threads[i].getState();
+			}
+			for (int i=0; i<10; i++){
+				threads[i].start();
+			}
+			boolean finish=false;
+			while (!finish) {
+			for (int i=0; i<10; i++){
+				if (threads[i].getState()!=stats[i]) {
+					writeThreadInfo(pw, threads[i],stats[i]);
+					stats[i]=threads[i].getState();
+				}
+			}
+			finish=true;
+			for (int i=0; i<10; i++){
+				finish=finish &&(threads[i].getState()==Thread.State.TERMINATED);
+			}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private static void writeThreadInfo(PrintWriter pw, Thread
+		thread, Thread.State state) {
+		pw.printf("Main : Id %d - %s\n",thread.getId(),thread.getName());
+		pw.printf("Main : Priority: %d\n",thread.getPriority());
+		pw.printf("Main : Old State: %s\n",state);
+		pw.printf("Main : New State: %s\n",thread.getState());
+		pw.printf("Main : ************************************\n");
 	}
 }
